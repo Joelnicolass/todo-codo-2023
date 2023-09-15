8386eb13176f2a5e3986b9dffc8fb29f06f59bf1
Joelnicolass <joelnicolass@gmail.com>
2023-09-15 12:17:30 -0300

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ToDo from "./components/ToDo/ToDo";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <ToDo />
+    </>
+  );
 };
 
 export default App;
